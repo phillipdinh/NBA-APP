98880bea2b952dad8a15092d4372fa5f9669eba6
phillipdinh <phillipdinh11@yahoo.com>
2024-03-07 14:39:27 -0800

    Prettier formatting

--- src/hooks/useAllPlayersSearch.js
+++ src/hooks/useAllPlayersSearch.js
@@ -2,41 +2,41 @@ import { useEffect } from "react"
 import { useDispatch } from "react-redux"
 
 import {
-    toggleSearchResultsLoading,
-    setSearchResults,
-    setSearchResultsError
+	toggleSearchResultsLoading,
+	setSearchResults,
+	setSearchResultsError
 } from "../redux/actions"
 
 import api from "../api"
 
 function useAllPlayersSearch(fetched, name) {
-    const dispatch = useDispatch()
+	const dispatch = useDispatch()
 
-    useEffect(() => {
-        const getAllPlayers = async () => {
-            dispatch(toggleSearchResultsLoading(true))
-            let search = []
-            if (name != "") {
-                try {
-                    console.log(`Searching for player with last name: ${name}`)
-                    const resp = await api.get(`players?search=${name}`)
-                    search = resp.data.response
-                    console.log(search)
-                    if (resp.status === 200) {
-                        dispatch(setSearchResults(search))
-                    } else {
-                        dispatch(setSearchResultsError(resp.data.errors[0]))
-                    }
-                } catch (e) {
-                    dispatch(setSearchResultsError(e.message))
-                    console.log("Error: ", e)
-                }
-            }
-        }
-        if (!fetched) {
-            getAllPlayers()
-        }
-    }, [name])
+	useEffect(() => {
+		const getAllPlayers = async () => {
+			dispatch(toggleSearchResultsLoading(true))
+			let search = []
+			if (name !== "") {
+				try {
+					console.log(`Searching for player with last name: ${name}`)
+					const resp = await api.get(`players?search=${name}`)
+					search = resp.data.response
+					console.log(search)
+					if (resp.status === 200) {
+						dispatch(setSearchResults(search))
+					} else {
+						dispatch(setSearchResultsError(resp.data.errors[0]))
+					}
+				} catch (e) {
+					dispatch(setSearchResultsError(e.message))
+					console.log("Error: ", e)
+				}
+			}
+		}
+		if (!fetched) {
+			getAllPlayers()
+		}
+	}, [name])
 }
 
 export default useAllPlayersSearch
--- src/hooks/usePlayerBioSearch.js
+++ src/hooks/usePlayerBioSearch.js
@@ -1,37 +1,33 @@
 import { useEffect } from "react"
 import { useDispatch } from "react-redux"
-import {
-    setPlayerBioLoading,
-    setPlayerBio,
-    setPlayerBioError
-} from "../redux/actions"
+import { setPlayerBioLoading, setPlayerBio, setPlayerBioError } from "../redux/actions"
 import api from "../api"
 
 function usePlayerBioSearch(fetched, id) {
-    const dispatch = useDispatch()
+	const dispatch = useDispatch()
 
-    useEffect(() => {
-        const getPlayerBio = async () => {
-            dispatch(setPlayerBioLoading(true))
-            let info = []
-            try {
-                console.log(`Making request for player ${id} bio`)
-                const resp = await api.get(`players?id=${id}`)
-                info = resp.data.response
-                if (resp.status === 200) {
-                    dispatch(setPlayerBio(info))
-                } else {
-                    dispatch(setPlayerBioError(resp.data.errors[0]))
-                }
-            } catch (e) {
-                dispatch(setPlayerBioError(e.message))
-                console.log("Error: ", e)
-            }
-        }
-        if (!fetched) {
-            getPlayerBio()
-        }
-    }, [])
+	useEffect(() => {
+		const getPlayerBio = async () => {
+			dispatch(setPlayerBioLoading(true))
+			let info = []
+			try {
+				console.log(`Making request for player ${id} bio`)
+				const resp = await api.get(`players?id=${id}`)
+				info = resp.data.response
+				if (resp.status === 200) {
+					dispatch(setPlayerBio(info))
+				} else {
+					dispatch(setPlayerBioError(resp.data.errors[0]))
+				}
+			} catch (e) {
+				dispatch(setPlayerBioError(e.message))
+				console.log("Error: ", e)
+			}
+		}
+		if (!fetched) {
+			getPlayerBio()
+		}
+	}, [])
 }
 
 export default usePlayerBioSearch
