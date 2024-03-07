e3e5f4d26580dbb23c86a08df1803d599583465c
phillipdinh <phillipdinh11@yahoo.com>
2024-03-06 21:18:03 -0800

    Make sure to return null

--- src/pages/Players.js
+++ src/pages/Players.js
@@ -7,100 +7,100 @@ import Spinner from "../components/Spinner"
 import SearchBar from "../components/SearchBar"
 import PlayerListItem from "../components/PlayerListItem"
 import {
-    getLoadingSearchResults,
-    getSearchResultsError,
-    getSearchResults
+	getLoadingSearchResults,
+	getSearchResultsError,
+	getSearchResults
 } from "../redux/selectors"
 import { useDispatch } from "react-redux"
 import { setSearchResults } from "../redux/actions"
 function Players(props) {
-    const dispatch = useDispatch()
-    const [searchInput, setSearchInput] = useState("")
+	const dispatch = useDispatch()
+	const [searchInput, setSearchInput] = useState("")
 
-    const loading = useSelector(getLoadingSearchResults)
-    const error = useSelector(getSearchResultsError)
+	const loading = useSelector(getLoadingSearchResults)
+	const error = useSelector(getSearchResultsError)
 
-    const searchResults = useSelector(getSearchResults)
-    useAllPlayersSearch(searchResults.length != 0, searchInput)
+	const searchResults = useSelector(getSearchResults)
+	useAllPlayersSearch(searchResults.length != 0, searchInput)
 
-    return (
-        <>
-            <PageContainer>
-                <SearchBar onSearch={handleSearch} />
+	return (
+		<>
+			<PageContainer>
+				<SearchBar onSearch={handleSearch} />
 
-                {loading ? (
-                    <Spinner />
-                ) : (
-                    <>
-                        {error != "" ? (
-                            <Notice message={error} error={true} />
-                        ) : (
-                            <PlayersContainer>
-                                {(searchResults || []).map((player) => {
-                                    if (hasPlayerData(player))
-                                        return (
-                                            <PlayerListItem
-                                                key={player.id}
-                                                player={player}
-                                            />
-                                        )
-                                })}
-                            </PlayersContainer>
-                        )}
-                    </>
-                )}
-            </PageContainer>
-        </>
-    )
+				{loading ? (
+					<Spinner />
+				) : (
+					<>
+						{error !== "" ? (
+							<Notice message={error} error={true} />
+						) : (
+							<PlayersContainer>
+								{(searchResults || []).map((player) => {
+									if (hasPlayerData(player)) {
+										return (
+											<PlayerListItem
+												key={player.id}
+												player={player}
+											/>
+										)
+									}
+									return null
+								})}
+							</PlayersContainer>
+						)}
+					</>
+				)}
+			</PageContainer>
+		</>
+	)
 
-    function handleSearch(input) {
-        dispatch(setSearchResults([]))
-        setSearchInput(input)
-    }
+	function handleSearch(input) {
+		dispatch(setSearchResults([]))
+		setSearchInput(input)
+	}
 }
 function hasPlayerData(player) {
-    return (
-        player.leagues.standard &&
-        player.leagues.standard.jersey &&
-        player.leagues.standard.pos &&
-        player.firstname &&
-        player.lastname &&
-        player.leagues.standard.active
-    )
+	return (
+		player.leagues.standard &&
+		player.leagues.standard.jersey &&
+		player.leagues.standard.pos &&
+		player.firstname &&
+		player.lastname &&
+		player.leagues.standard.active
+	)
 }
 
 function PlayersContainer(props) {
-    const hasChildren = Children.count(props.children) > 0
+	const hasChildren = Children.count(props.children) > 0
 
-    return (
-        <Container>
-            {hasChildren ? props.children : <h2>No search results</h2>}
-        </Container>
-    )
+	return (
+		<Container>{hasChildren ? props.children : <h2>No search results</h2>}</Container>
+	)
 }
 
 const Container = styled.div`
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    background-color: white;
-    width: 80%;
-    padding: 20px;
-    align-items: center;
-    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.4);
-    border-radius: 4px;
+	flex: 1;
+	display: flex;
+	flex-direction: column;
+	background-color: white;
+	width: 80%;
+	padding: 20px;
+	align-items: center;
+	box-shadow: 0 2px 8px rgba(0, 0, 0, 0.4);
+	border-radius: 4px;
 `
 
 const PageContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    height: 100vh;
-    gap: 1em;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	height: 100vh;
+	gap: 1em;
 `
 
 const PageTitle = styled.h1`
-    font-size: 40px;
-    font-weight: 800;
+	font-size: 40px;
+	font-weight: 800;
 `
 export default Players
